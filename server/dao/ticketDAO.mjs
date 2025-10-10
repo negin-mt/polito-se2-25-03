465d40d26e7e13f4cc8cd522138c36d95d2b6808
EdoardoCotto <s343449@studenti.polito.it>
2025-10-10 18:53:56 +0200

    Start ticketDAO

---
+++ server/dao/ticketDAO.mjs
@@ -0,0 +1,42 @@
+import sqlite from 'sqlite3';
+
+const db = new sqlite.Database('database.sqlite', (err) => {
+    if (err) throw err;
+});
+
+//TICKET
+export const addTicket = async (ticket_number, service_type_id, status, counter_id, queue_position, issued_at, called_at, completed_at, cancelled_at, notes) => {
+    return new Promise((resolve, reject) => {
+        const sql = `INSERT INTO tickets (ticket_number, service_type_id, status, counter_id, 
+                                                queue_position, issued_at, called_at, completed_at, cancelled_at, notes) 
+                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+        db.run(sql, [ticket_number, service_type_id, status, counter_id, queue_position, issued_at, called_at, completed_at, cancelled_at, notes],
+            function (err) {
+                if (err) {
+                    reject(err);
+                }
+                else {
+                    resolve(this.lastID);
+                }
+            });
+    });
+}
+
+export const getTicketById = async (ticket_id) => {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT * 
+                            FROM tickets
+                            WHERE ticket_id = ?;`;
+        db.get(sql, [ticket_id], (err, row) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(row);
+            }
+        });
+    })
+}
+
+
+
